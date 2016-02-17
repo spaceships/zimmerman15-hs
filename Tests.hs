41e4c6bd9f19f1bd0cbf7f6250adadcb1cea1d7e
Brent Carmer <bcarmer@gmail.com>
2016-02-17 13:58:27 -0500

    Add a top-level zeroTest test

--- Tests.hs
+++ Tests.hs
@@ -1,6 +1,13 @@
 module Main where
 
 import Zim14.Index
+import Zim14.Circuit
+import Zim14.Circuit.Parser
+import Zim14.Encoding.CLT13
+import Zim14.Util
+
+import CLT13.Rand
+import qualified CLT13 as CLT
 
 import Data.Monoid
 import Test.Framework
@@ -16,54 +23,26 @@ import Debug.Trace
 main :: IO ()
 main = defaultMainWithOpts tests mempty { ropt_color_mode = Just ColorAlways }
 
-tests = [ --testProperty "all the indices are used" prop_allIndicesUsed
-        {-, testProperty "xindices correctly returns all xindices" prop_xindicesCorrect-}
-        {-, testProperty "zindices correctly returns all zindices" prop_zindicesCorrect-}
-        {-, testProperty "windices correctly returns all windices" prop_windicesCorrect-}
-        {-, testProperty "sindices correctly returns all sindices" prop_sindicesCorrect-}
+tests = [ testProperty "top level encoding zero tests correctly" prop_zeroTestTopLevel
         ]
 
--- there is a weak point translating between Index and CLT.IndexSet
+prop_zeroTestTopLevel :: Property
+prop_zeroTestTopLevel = monadicIO $ do
+    x <- pick arbitrary
+    y <- pick arbitrary
+    λ <- pick $ choose (4, 16)
+    z <- run $ zeroTestTopLevel "circuits/add.acirc" λ (b2i x) (b2i y)
+    let isZero False False = True
+        isZero _ _         = False
+    assert (isZero x y == z)
 
-{-prop_allIndicesUsed :: Property-}
-{-prop_allIndicesUsed = forAll (choose (1, 128)) $ \n ->-}
-    {-let ixs = topLevelIndex n-}
-        {-nzs = numCLTIndices n-}
-    {-in S.fromList [0..nzs-1] == S.fromList (M.keys (indexer n ixs))-}
-
-{-allIndices :: Int -> Index-}
-{-allIndices n = mconcat $ map pow1 (y : xs ++ zs ++ ws ++ ss)-}
-  {-where-}
-    {-y  = Y-}
-    {-xs = [(X i b)   | b <- [False, True], i <- [0..n-1]]-}
-    {-zs = [(Z i)     | i <- [0..n-1]]-}
-    {-ws = [(W i)     | i <- [0..n-1]]-}
-    {-ss = [(S i b j) | b <- [False, True], i <- [0..n-1], j <- [0..n-1], i < j]-}
-
-{-prop_xindicesCorrect :: Property-}
-{-prop_xindicesCorrect = forAll (choose (1, 128)) $ \n ->-}
-    {-let ix  = indexer n-}
-        {-xs  = concat [ix (X i b) | b <- [False, True], i <- [0..n-1]]-}
-        {-xs' = xindices n-}
-    {-in S.fromList xs == S.fromList xs'-}
-
-{-prop_zindicesCorrect :: Property-}
-{-prop_zindicesCorrect = forAll (choose (1, 128)) $ \n ->-}
-    {-let ix  = indexer n-}
-        {-zs  = concat [ix (Z i) | i <- [0..n-1]]-}
-        {-zs' = zindices n-}
-    {-in S.fromList zs == S.fromList zs'-}
-
-{-prop_windicesCorrect :: Property-}
-{-prop_windicesCorrect = forAll (choose (1, 128)) $ \n ->-}
-    {-let ix  = indexer n-}
-        {-ws  = concat [ix (W i) | i <- [0..n-1]]-}
-        {-ws' = windices n-}
-    {-in S.fromList ws == S.fromList ws'-}
-
-{-prop_sindicesCorrect :: Property-}
-{-prop_sindicesCorrect = forAll (choose (1, 128)) $ \n ->-}
-    {-let ix  = indexer n-}
-        {-ss  = concat [ix (S i b j) | b <- [False, True], i <- [0..n-1], j <- [0..n-1]]-}
-        {-ss' = sindices n-}
-    {-in S.fromList ss == S.fromList ss'-}
+zeroTestTopLevel :: FilePath -> Int -> Integer -> Integer -> IO Bool
+zeroTestTopLevel fp λ x y = do
+    (c, _) <- parseCirc <$> readFile fp
+    let n = ninputs c
+        d = depth c
+    mmap <- CLT.setup True (λ+d) d (numIndices n) (topLevelCLTIndex c)
+    let pp  = CLT.publicParams mmap
+        enc = cltEncode mmap (indexer c)
+    x <- randIO (enc x y (topLevelIndex c))
+    return $ CLT.isZero pp (getCLT x)
